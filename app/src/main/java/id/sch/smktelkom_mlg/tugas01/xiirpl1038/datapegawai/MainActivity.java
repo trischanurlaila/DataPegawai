a8bf10a2ff8c02f74692a1cc1e14c106bdb11fab
TrischaNL <l.trischa@gmail.com>
2016-09-13 18:08:37 -0700

    + method doProcess, doClick, doCheck, doSelect + method isValid untuk uji kesalahan

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1038/datapegawai/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1038/datapegawai/MainActivity.java
@@ -54,5 +54,132 @@ public class MainActivity extends AppCompatActivity
             tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
             tvHasil3 = (TextView) findViewById(R.id.textViewHasil3);
             tvHasil4 = (TextView) findViewById(R.id.textViewHasil4);
+
+            adapter = new BagianAdapter(this, listBagian);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spBagian.setAdapter(adapter);
+
+            spJabatan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                    listBagian.clear();
+                    listBagian.addAll(Arrays.asList(arBagian[pos]));
+                    adapter.setJabatan((String)spJabatan.getItemAtPosition(pos));
+                    adapter.notifyDataSetChanged();
+                    spBagian.setSelection(0);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+            bOK.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    doProcess();
+                    doClick();
+                    doCheck();
+                    doSelect();
+
+                }
+            });
+
+            rgStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int i) {
+                    if(i == R.id.radioButtonBM)
+                    {
+                        findViewById(R.id.tilJA).setVisibility(View.GONE);
+                    }
+                    else
+                    {
+                        findViewById(R.id.tilJA).setVisibility(View.VISIBLE);
+                    }
+                }
+            });
         }
+
+
+        private void doSelect() {
+            tvHasil4.setText("Jabatan : " + spJabatan.getSelectedItem().toString()
+                    +" bagian "+spBagian.getSelectedItem().toString());
+        }
+
+        private void doCheck() {
+            String ahli = "Bidang Keahlian: ";
+            int startlen = ahli.length();
+            if(cbP.isChecked()) ahli+=cbP.getText()+" ";
+            if(cbI.isChecked()) ahli+=cbI.getText()+" ";
+            if(cbT.isChecked()) ahli+=cbT.getText()+" ";
+
+            if(ahli.length()==startlen) ahli+="Tidak ada pada pilihan";
+            tvHasil3.setText(ahli);
+        }
+
+        private void doClick() {
+            String status = null;
+            if (rgStatus.getCheckedRadioButtonId()!=-1)
+            {
+                RadioButton rb = (RadioButton) findViewById(rgStatus.getCheckedRadioButtonId());
+                status = rb.getText().toString();
+
+                if(rgStatus.getCheckedRadioButtonId()!=R.id.radioButtonBM)
+                {
+                    EditText etJA = (EditText) findViewById(R.id.editTextJA);
+                    status += " dengan anak berjumlah : " + etJA.getText();
+                }
+            }
+
+            if (status == null)
+            {tvHasil2.setText("Belum memiih Status");}
+            else
+            {tvHasil2.setText("Status Anda : " +status);}
+        }
+
+        private void doProcess() {
+            if(isValid())
+            {
+                String nama = etNama.getText().toString();
+                String pengalaman = etPengalaman.getText().toString();
+                tvHasil.setText("\nNama : " + nama + "\nPengalaman Kerja : " + pengalaman + " tahun");
+            }
+        }
+
+        private boolean isValid() {
+            boolean valid = true;
+
+            String nama = etNama.getText().toString();
+            String pengalaman = etPengalaman.getText().toString();
+
+            if(nama.isEmpty())
+            {
+                etNama.setError("Nama belum diisi");
+                valid = false;
+            }
+            else if(nama.length()<3)
+            {
+                etNama.setError("Nama minimal 3 karakter");
+                valid = false;
+            }
+            else
+            {
+                etNama.setError(null);
+            }
+
+            if(pengalaman.isEmpty())
+            {
+                etPengalaman.setError("Tahun pengalaman belum diisi");
+                valid = false;
+            }
+            else
+            {
+                etPengalaman.setError(null);
+            }
+
+            return valid;
+        }
+
     }
+
